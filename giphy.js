2903807d01a2adb6c4f32435cd442780a40586e8
lrdkelvin <brandonbringhurst@gmail.com>
2020-04-28 15:43:05 -0600

    added ability to click on a picture and it will start moving

--- giphy.js
+++ giphy.js
@@ -1,4 +1,4 @@
-var topics = ["dogs", "cats", "horses", "gerbals", ];
+var topics = ["dogs", "cats", "horses", "gerbals", "wolves", "stubborn"];
 
 
 
@@ -21,18 +21,85 @@ $.ajax({
 
     createimg();
 
-    $("#image" + i).attr("src", response.data[i].images.preview_gif.url);
-    $("#image" + i).attr("width", "250")
-    $("#image" + i).attr("height", "250")
-    $("#image" + i)
+    $("#image" + i).attr("src", response.data[i].images.original_still.url);
+    $("#image" + i).attr("width", "250");
+    $("#image" + i).attr("height", "250");
     console.log("yes");
     
   }
 
- 
+  $("#image0").on("click", function() {
+    $("#image0").attr("src", response.data[0].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
 });
 
+$("#image1").on("click", function() {
+    $("#image1").attr("src", response.data[1].images.preview_gif.url);
+    console.log( "you clicked it!");
 
+    
+});
+
+$("#image2").on("click", function() {
+    $("#image2").attr("src", response.data[2].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+$("#image3").on("click", function() {
+    $("#image3").attr("src", response.data[3].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+$("#image4").on("click", function() {
+    $("#image4").attr("src", response.data[4].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+$("#image5").on("click", function() {
+    $("#image5").attr("src", response.data[5].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+$("#image6").on("click", function() {
+    $("#image6").attr("src", response.data[6].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+$("#image7").on("click", function() {
+    $("#image7").attr("src", response.data[7].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+$("#image8").on("click", function() {
+    $("#image8").attr("src", response.data[8].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+$("#image9").on("click", function() {
+    $("#image9").attr("src", response.data[9].images.preview_gif.url);
+    console.log( "you clicked it!");
+
+    
+});
+
+ 
+});
 
 
 
